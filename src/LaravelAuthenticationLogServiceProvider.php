99a594fab322867778429893a00691da3bb82c05
Jack <jjbayliss@icloud.com>
2025-06-26 00:28:31 +0100

    Update LaravelAuthenticationLogServiceProvider.php
    idiomatic

--- src/LaravelAuthenticationLogServiceProvider.php
+++ src/LaravelAuthenticationLogServiceProvider.php
@@ -27,10 +27,9 @@ class LaravelAuthenticationLogServiceProvider extends PackageServiceProvider
             ->hasMigration('create_authentication_log_table')
             ->hasCommand(PurgeAuthenticationLogCommand::class);
 
-        $events = $this->app->make(Dispatcher::class);
-        $events->listen(config('authentication-log.events.login', Login::class), config('authentication-log.listeners.login', LoginListener::class));
-        $events->listen(config('authentication-log.events.failed', Failed::class), config('authentication-log.listeners.failed', FailedLoginListener::class));
-        $events->listen(config('authentication-log.events.logout', Logout::class), config('authentication-log.listeners.logout', LogoutListener::class));
-        $events->listen(config('authentication-log.events.other-device-logout', OtherDeviceLogout::class), config('authentication-log.listeners.other-device-logout', OtherDeviceLogoutListener::class));
+        app('events')->listen(config('authentication-log.events.login', Login::class), config('authentication-log.listeners.login', LoginListener::class));
+        app('events')->listen(config('authentication-log.events.failed', Failed::class), config('authentication-log.listeners.failed', FailedLoginListener::class));
+        app('events')->listen(config('authentication-log.events.logout', Logout::class), config('authentication-log.listeners.logout', LogoutListener::class));
+        app('events')->listen(config('authentication-log.events.other-device-logout', OtherDeviceLogout::class), config('authentication-log.listeners.other-device-logout', OtherDeviceLogoutListener::class));
     }
 }
