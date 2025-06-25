f83c2f60c8507bfc37606639becf0ff78597d945
Jack <jjbayliss@icloud.com>
2025-06-26 00:33:40 +0100

    Update LaravelAuthenticationLogServiceProvider.php

--- src/LaravelAuthenticationLogServiceProvider.php
+++ src/LaravelAuthenticationLogServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace Rappasoft\LaravelAuthenticationLog;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Failed;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Auth\Events\OtherDeviceLogout;
-use Illuminate\Contracts\Events\Dispatcher;
 use Rappasoft\LaravelAuthenticationLog\Commands\PurgeAuthenticationLogCommand;
 use Rappasoft\LaravelAuthenticationLog\Listeners\FailedLoginListener;
 use Rappasoft\LaravelAuthenticationLog\Listeners\LoginListener;
@@ -27,9 +27,9 @@ class LaravelAuthenticationLogServiceProvider extends PackageServiceProvider
             ->hasMigration('create_authentication_log_table')
             ->hasCommand(PurgeAuthenticationLogCommand::class);
 
-        app('events')->listen(config('authentication-log.events.login', Login::class), config('authentication-log.listeners.login', LoginListener::class));
-        app('events')->listen(config('authentication-log.events.failed', Failed::class), config('authentication-log.listeners.failed', FailedLoginListener::class));
-        app('events')->listen(config('authentication-log.events.logout', Logout::class), config('authentication-log.listeners.logout', LogoutListener::class));
-        app('events')->listen(config('authentication-log.events.other-device-logout', OtherDeviceLogout::class), config('authentication-log.listeners.other-device-logout', OtherDeviceLogoutListener::class));
+        Event::listen(config('authentication-log.events.login', Login::class), config('authentication-log.listeners.login', LoginListener::class));
+        Event::listen(config('authentication-log.events.failed', Failed::class), config('authentication-log.listeners.failed', FailedLoginListener::class));
+        Event::listen(config('authentication-log.events.logout', Logout::class), config('authentication-log.listeners.logout', LogoutListener::class));
+        Event::listen(config('authentication-log.events.other-device-logout', OtherDeviceLogout::class), config('authentication-log.listeners.other-device-logout', OtherDeviceLogoutListener::class));
     }
 }
